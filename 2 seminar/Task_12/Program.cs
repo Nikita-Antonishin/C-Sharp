03de3aa77785ac1063e08e8439b696c466c210ac
Nikita <antonishin.nikitka@mail.ru>
2022-12-14 00:30:44 +0500

    Z_12

---
+++ 2 seminar/Task_12/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Число: ");
+int num = int.Parse(Console.ReadLine()!);
+if (num < 0)
+{
+    num = -num;
+}
+if (num < 100)
+{
+    Console.WriteLine("Третьей цифры нету");
+}
+else
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    int thierdDigit = num % 10;
+    Console.WriteLine(thierdDigit);
+}
+
